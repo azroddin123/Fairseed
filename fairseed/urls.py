f30a985a5ee5dd81cf8b64e713803e55e3a3d090
Azroddin Mujawar <33azharoddin@gmail.com>
2023-12-12 10:56:26 +0530

    Removed Authentication

--- fairseed/urls.py
+++ fairseed/urls.py
@@ -34,9 +34,9 @@ urlpatterns = [
 
 
     # Token URLs
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/token/',TokenObtainPairView.as_view(),name="token_obtain_pair"),
-    path('api/token/refresh/',TokenRefreshView.as_view(),name="token_refresh")
+    # path('api-auth/', include('rest_framework.urls')),
+    # path('api/token/',TokenObtainPairView.as_view(),name="token_obtain_pair"),
+    # path('api/token/refresh/',TokenRefreshView.as_view(),name="token_refresh")
 ]
 
 from django.contrib import admin
