f5aa449e95fb8c5cf96804f0a5bfe9004afb4981
Azroddin Mujawar <33azharoddin@gmail.com>
2024-01-08 16:24:20 +0530

    Added vercel configurations

--- fairseed/settings.py
+++ fairseed/settings.py
@@ -153,11 +153,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import os 
 
-
-CRONJOBS = [
-    ('*/1 * * * *', 'portals.cronjob.update_campaign_fund')
-]
-
-ALLOWED_HOSTS = ['.vercel.app']
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 CORS_ORIGIN_ALLOW_ALL = True
