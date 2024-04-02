915a6f7cc62fe0bda17bfdd2150612f3332b7aff
Azhar M <33azharoddin@gmail.com>
2024-04-02 14:00:08 +0530

    Commiting Changes

--- campaigns/views.py
+++ campaigns/views.py
@@ -176,7 +176,6 @@ class CampaignTabsAPi(APIView):
             ).order_by('difference_percentage')
             else :
                 data = Campaign.objects.filter(status="Active")
-         
             response = paginate_data(Campaign, CampaignAdminSerializer, request, data)
             response["filter_key"] = filter_key
             return Response(response, status=status.HTTP_200_OK)
@@ -212,12 +211,13 @@ class CampaignTabsAPi2(APIView):
             ).order_by('difference_percentage')
             else :
                 data = Campaign.objects.filter(status="Active",category=cat_id)
-            
             print(filter_key,"====================")
             response = paginate_data(Campaign, CampaignAdminSerializer, request, data)
+            response["filter_key"] = filter_key
             return Response(response, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"error": True, "message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class AddCampaignApi(APIView):
     def post(self,request,*args, **kwargs):
@@ -241,4 +241,3 @@ class AddCampaignApi(APIView):
                         return Response({"error" : False, "message" : "Campaign Data Saved Successfully" , "data" : campaign_serializer.data, "id" : campaign.id},status=status.HTTP_200_OK)
         except Exception as e :
             return Response({"error" : True , "message" : str(e)},status=status.HTTP_400_BAD_REQUEST)
-
