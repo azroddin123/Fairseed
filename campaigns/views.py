353172795c9e6cb3f6b2d27d5ed9692373d2af99
Azhar M <33azharoddin@gmail.com>
2024-04-18 12:14:10 +0530

    Added Successful Camapign

--- campaigns/views.py
+++ campaigns/views.py
@@ -82,7 +82,7 @@ class SuccessfulCauseApi(APIView):
     def get(self,request,*args, **kwargs) :
         try :
             data = Campaign.objects.filter(is_successful=True)
-            response = paginate_data(Campaign,CampaignAdminSerializer,request,data)
+            response = paginate_data(model=Campaign,serializer=CampaignAdminSerializer,request=request,data=data)
             return Response(response,status=status.HTTP_200_OK)
         except Exception as e :
             return Response({"error" : str(e) },status=status.HTTP_400_BAD_REQUEST)
