6e502ebf56fd506b811a43268c9838e71b1b3e5b
Azhar M <33azharoddin@gmail.com>
2024-05-06 18:45:15 +0530

    Added New Method

--- portals/services.py
+++ portals/services.py
@@ -110,20 +110,38 @@ def paginate_model_data(model, serializer, request, filter_key=None):
     except Exception as e:
         return Response({"error": True, "message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
     
-def paginate_data(model, serializer, request,data):
+# def paginate_data(model, serializer, request,data):
+#     try:
+#         limit = max(int(request.GET.get('limit', 1)), 1)
+#         page_number = max(int(request.GET.get('page', 1)), 1)
+#         data = data
+#         paginator = Paginator(data, limit)
+#         try:
+#             current_page_data = paginator.get_page(page_number)
+#         except EmptyPage:
+#             return Response({"error": True, "message": "Page not found"}, status=status.HTTP_404_NOT_FOUND)
+#         try:
+#             serialized_data = serializer(current_page_data, many=True).data
+#         except Exception as e:
+#             return Response({"error": True, "message": f"Serialization error: {str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
+#         response_data = {
+#             "error": False,
+#             "pages_count": paginator.num_pages,
+#             "count": paginator.count,
+#             "rows": serialized_data
+#         }
+#         return response_data
+#     except Exception as e:
+#         return Response({"error": True, "message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    
+
+def paginate_data(model, serializer, request, data):
     try:
-        limit = max(int(request.GET.get('limit', 1)), 1)
+        limit = max(int(request.GET.get('limit', 10)), 1)  # Default limit is 10 if not provided or invalid
         page_number = max(int(request.GET.get('page', 1)), 1)
-        data = data
         paginator = Paginator(data, limit)
-        try:
-            current_page_data = paginator.get_page(page_number)
-        except EmptyPage:
-            return Response({"error": True, "message": "Page not found"}, status=status.HTTP_404_NOT_FOUND)
-        try:
-            serialized_data = serializer(current_page_data, many=True).data
-        except Exception as e:
-            return Response({"error": True, "message": f"Serialization error: {str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
+        current_page_data = paginator.get_page(page_number)
+        serialized_data = serializer(current_page_data, many=True).data
         response_data = {
             "error": False,
             "pages_count": paginator.num_pages,
@@ -133,5 +151,6 @@ def paginate_data(model, serializer, request,data):
         return response_data
     except Exception as e:
         return Response({"error": True, "message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
     
     
