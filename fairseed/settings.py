9d7009144c320168213a168727f6dcfd4d4c4f6c
HuzaifaAnsari2806 <ansarihuzaifa2806@gmail.com>
2024-08-23 12:53:46 +0530

    forgot to change db

--- fairseed/settings.py
+++ fairseed/settings.py
@@ -108,26 +108,26 @@ TEMPLATES = [
 WSGI_APPLICATION = 'fairseed.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'timeout': 10,  # Wait up to 10 seconds for a lock
-        },
-    }
-}
-
 # DATABASES = {
-# 	'default': {
-# 		'ENGINE': 'django.db.backends.mysql',
-# 		'NAME': 'fairseed_db',
-# 		'USER': 'root',
-# 		'PASSWORD': 'password',
-# 		'HOST':'localhost',
-# 		'PORT':'3306',
-# 	}
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'OPTIONS': {
+#             'timeout': 10,  # Wait up to 10 seconds for a lock
+#         },
+#     }
 # }
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'fairseed_db',
+		'USER': 'root',
+		'PASSWORD': 'password',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
