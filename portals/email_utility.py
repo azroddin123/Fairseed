9c2fb79a9331815eee1fd86b0eb979b11ed827f2
unknown <ansarihuzaifa2806@gmail.com>
2024-09-30 18:54:17 +0530

    email temolate modified

--- portals/email_utility.py
+++ portals/email_utility.py
@@ -1,16 +1,17 @@
 # email_utils.py
 import threading
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from django.conf import settings
 
 def send_email(subject, message, recipient_list):
-    send_mail(
-        subject,
-        message,
-        settings.EMAIL_HOST_USER,
-        recipient_list,
-        fail_silently=False,
-    )
+        email = EmailMessage(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            recipient_list,
+        )
+        email.content_subtype = "html"  # Set content type to HTML
+        email.send(fail_silently=False)
 
 class EmailThread(threading.Thread):
     def __init__(self, subject, message, recipient_list):
