fc12470b08ab4383077f7221886bdd1733055a1a
unknown <ansarihuzaifa2806@gmail.com>
2024-10-11 12:12:12 +0530

    donor count logic

--- donors/views.py
+++ donors/views.py
@@ -46,8 +46,8 @@ class DonatePaymentApi(APIView):
                     merchant_id = settings.PROD_MERCHANT_ID
                     salt_key = settings.PROD_SALT_KEY   
                     salt_index = settings.PROD_SALT_INDEX
-                    env = Env.PROD 
-                    # env = Env.UAT 
+                    # env = Env.PROD 
+                    env = Env.UAT 
 
                     phonepe_client = PhonePePaymentClient(merchant_id=merchant_id, salt_key=salt_key, salt_index=salt_index, env=env)
                     unique_transaction_id = str(uuid.uuid4())[:-2]
@@ -210,8 +210,8 @@ def update_transaction(unique_transaction_id, amount):
         merchant_id = settings.PROD_MERCHANT_ID
         salt_key = settings.PROD_SALT_KEY
         salt_index = settings.PROD_SALT_INDEX
-        env = Env.PROD
-        # env = Env.UAT
+        # env = Env.PROD
+        env = Env.UAT
 
         phonepe_client = PhonePePaymentClient(merchant_id=merchant_id, salt_key=salt_key, salt_index=salt_index, env=env)
         response = phonepe_client.check_status(merchant_transaction_id=unique_transaction_id)
