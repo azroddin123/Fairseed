2d30e46219ccd6b97c08083ebb4a0d7b6f3e63d0
unknown <ansarihuzaifa2806@gmail.com>
2024-12-11 17:07:44 +0530

    addded seurity(.env)

--- fairseed/settings.py
+++ fairseed/settings.py
@@ -9,20 +9,26 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
 import pymysql
+import os
+
 pymysql.install_as_MySQLdb()
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-ADMIN_SITE_TITLE = 'FairSeed Admin Panel'
-ADMIN_SITE_HEADER = 'FairSeed Admin'
+ADMIN_SITE_TITLE = "FairSeed Admin Panel"
+ADMIN_SITE_HEADER = "FairSeed Admin"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gw^-zlx%6zd$iso&-7iiixo1z66^%c_dvgaj3=4=&pwiawuaf%'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -30,82 +36,81 @@ DEBUG = True
 CORS_ALLOW_HEADERS = [
     "*",
 ]
-ALLOWED_HOSTS = ["*",]
+ALLOWED_HOSTS = [
+    "*",
+]
 
-AUTH_USER_MODEL = 'accounts.User'
+AUTH_USER_MODEL = "accounts.User"
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # Apps
-    'accounts',
-    'admin_dashboard',
-    'donors',
-    'campaigns',
-    'payment_gateways',
-    'portals',
-    # Packages 
-    'rest_framework',
-    'rest_framework_simplejwt',
-    'corsheaders',
-    'django_seed',
-    'django_celery_beat',
-    'redis',
-    'tinymce',
-
-    ]
+    "accounts",
+    "admin_dashboard",
+    "donors",
+    "campaigns",
+    "payment_gateways",
+    "portals",
+    # Packages
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "corsheaders",
+    "django_seed",
+    "django_celery_beat",
+    "redis",
+    "tinymce",
+]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ]
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,  # Set the default number of items per page for page-based pagination
-    'DEFAULT_LIMIT': 20,  # Set the default limit for limit-based pagination
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,  # Set the default number of items per page for page-based pagination
+    "DEFAULT_LIMIT": 20,  # Set the default limit for limit-based pagination
 }
 
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'portals.middlewares.CustomAuthentication'
-    
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "portals.middlewares.CustomAuthentication",
 ]
-ROOT_URLCONF = 'fairseed.urls'
+ROOT_URLCONF = "fairseed.urls"
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
-WSGI_APPLICATION = 'fairseed.wsgi.application'
+WSGI_APPLICATION = "fairseed.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 # DATABASES = {
@@ -119,34 +124,34 @@ WSGI_APPLICATION = 'fairseed.wsgi.application'
 # }
 
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'fairseed_db',
-		'USER': 'root',
-		'PASSWORD': 'password',
-		'HOST':'localhost',
-		'PORT':'3306',
-	}
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("NAME"),
+        "USER": os.getenv("USER"),
+        "PASSWORD": os.getenv("PASSWORD"),
+        "HOST": os.getenv("HOST"),
+        "PORT": os.getenv("PORT"),
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 # settings.py
 USE_I18N = True
 # USE_TZ = True
@@ -156,54 +161,39 @@ USE_I18N = True
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR.joinpath("media/")
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR.joinpath('static/')
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR.joinpath("static/")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-FILE_UPLOAD_MAX_MEMORY_SIZE = 10 * 1024 * 1024 # 10 Mb limit
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+FILE_UPLOAD_MAX_MEMORY_SIZE = 10 * 1024 * 1024  # 10 Mb limit
 
-import os 
-# STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 CORS_ORIGIN_ALLOW_ALL = True
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_HOST_USER = '33azharoddin@gmail.com'
-# EMAIL_HOST_PASSWORD = 'tmhzemmcwirzimmc'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True 
-# EMAIL_USE_SSL = False
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = 'info@fairseed.org'
-EMAIL_HOST_PASSWORD = 'yuzddbbwnldaxxdk'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True 
+EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
 
-
-#configured for celery beat
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler' 
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Asia/Kolkata'
+# configured for celery beat
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TASK_SERIALIZER = "json"
+CELERY_TIMEZONE = "Asia/Kolkata"
 CELERY_WORKER_CONCURRENCY = 4
 
 
-PROD_MERCHANT_ID = "FAIRSEEDONLINE"   
-PROD_SALT_KEY    = "fe43ebc9-626b-4dc3-8d4f-fa28b20846b9"    
-PROD_SALT_INDEX  = 1 
-REDIRECT_URL     ="https://fairseed.org"
+PROD_MERCHANT_ID = os.getenv("PROD_MERCHANT_ID")
+PROD_SALT_KEY = os.getenv("PROD_SALT_KEY")
+PROD_SALT_INDEX = os.getenv("PROD_SALT_INDEX")
+REDIRECT_URL = "https://fairseed.org"
 
-
-# PROD_MERCHANT_ID = "PGTESTPAYUAT100"   
-# PROD_SALT_KEY    = "cc2f75ad-01c2-4417-92f8-32964ce8d12d"    
-# PROD_SALT_INDEX  = 1 
 # REDIRECT_URL     ="https://dev.fairseed.org"
